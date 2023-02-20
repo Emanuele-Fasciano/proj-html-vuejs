798fedeffca419c7b0a46c023fc3dddfbe3a5c26
Emanuele-Fasciano <emanuelefasciano@gmail.com>
2023-02-20 16:23:35 +0100

    bozza layout

---
+++ js/main.js
@@ -0,0 +1,11 @@
+ const { createApp } = Vue
+
+  createApp({
+    data() {
+      return {
+        icons: [
+           '<i class="fa-brands fa-facebook"></i>'
+        ]
+      }
+    }
+  }).mount('#app')
